2a33aec30ebfee619f8eda5620d6203e21f4fd5d
anatoly <britkov_02@mail.ru>
2023-03-19 22:24:30 +0300

    add white box testing for task 2

--- src/main/kotlin/task2/Task2.kt
+++ src/main/kotlin/task2/Task2.kt
@@ -3,29 +3,31 @@ package task2
 class Task2(var array: Array<Int>) {
     var radix : Int = 1
 
-    private fun sortStep() {
-        val counter : IntArray = IntArray(10) { 0 }
-        val output : IntArray = IntArray(array.size) { 0 }
-        array.forEach { counter[it / radix % 10]++ }
-        for (idx in 1..9) {
-            counter[idx] += counter[idx - 1]
-        }
-        for (idx in array.size - 1 downTo 0) {
-            val cntIdx = array[idx] / radix % 10
-            output[--counter[cntIdx]] = array[idx]
-        }
-        for (idx in 0..array.size - 1) {
-            array[idx] = output[idx]
-        }
-    }
-
     fun radixSort() : Array<Int> {
         val maxElement = array.max()
-        println(maxElement)
         while (maxElement / radix > 0) {
-            sortStep()
+            array = sortStep(array, radix)
             radix *= 10
         }
         return array
+    }
+
+    companion object {
+        fun sortStep(arr: Array<Int>, radix: Int) : Array<Int> {
+            val counter : IntArray = IntArray(10) { 0 }
+            val output : IntArray = IntArray(arr.size) { 0 }
+            arr.forEach { counter[it / radix % 10]++ }
+            for (idx in 1..9) {
+                counter[idx] += counter[idx - 1]
+            }
+            for (idx in arr.size - 1 downTo 0) {
+                val cntIdx = arr[idx] / radix % 10
+                output[--counter[cntIdx]] = arr[idx]
+            }
+            for (idx in 0..arr.size - 1) {
+                arr[idx] = output[idx]
+            }
+            return arr;
+        }
     }
 }
